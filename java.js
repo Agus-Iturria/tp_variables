a035ed68ffb54dde9dbfe0d7320325c91a45bc7a
Agus-Iturria <agustin.iturria@gm2dev.com>
2024-07-10 13:01:42 -0300

    feat(java.js): upload exercises 1,2,3,4,5,6,7 & 8

---
+++ java.js
@@ -0,0 +1,51 @@
+//TP VARIABLES
+//Ejercicio 1
+let nombre = "Ricardo"
+//Ejercicio 2
+let DNI = 22334455
+//Ejercicio 3
+let boleano = Boolean
+//Ejercicio 4
+let saludo = "Hola soy "
+let nombre2 = "Ricardo Andres"
+let saludo_y_nombre = saludo + nombre2
+console.log(saludo_y_nombre)
+//Ejercicio 5
+let numero1 = 5
+let numero2 = 2
+//A)
+let suma = numero1 + numero2
+//B)
+let resta = numero1 - numero2
+//C)
+let multiplicacion = numero1 * numero2
+//D)
+let division = numero1 / numero2
+//E)
+let modulo = numero1 % numero2
+
+//Ejercicio 6
+let num1 = 34
+let str = "Edad: "
+let syn = str +  num1
+console.log(typeof syn) //la tercer variable es de tipo String
+
+//Ejercicio 7
+let si = true
+let no =  false
+
+//Ejercicio 8
+//A)
+let num2 = 2
+num2 = num2 + 1
+//B)
+let sum = +1
+num2 = num2 + sum
+//C)
+num2 = num2 - 1
+num2 = num2 * 2
+num2 = num2 / 2 
+
+//Ejercicio 9
+ 
+
